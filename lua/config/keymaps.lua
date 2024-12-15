2a75f4510b2b3420e7494b0b161f059b2225546b
theetah <phantomporpoise@gmail.com>
2024-12-15 17:05:09 -0600

    add a couple of missing plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -34,4 +34,6 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
-vim.keymap.set("n", "<Space>bd", "<cmd>bdelete!<CR>", { desc = "Delete the current buffer" })
+vim.keymap.set("n", "<leader>bd", "<cmd>bdelete!<CR>", { desc = "Delete the current buffer" })
+
+vim.keymap.set("n", "<leader>fp", "<cmd>ProjectMgr<CR>", { desc = "Open the project manager" })
