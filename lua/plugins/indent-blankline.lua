a200ab3305e4661572b07356bef1cee4940cbeaf
theetah <phantomporpoise@gmail.com>
2024-12-17 19:37:14 -0600

    Add indent-blankline.lua, rm redundant config file

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,16 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  config = function()
+    -- Kind of messy but it works
+    local hooks = require("ibl.hooks")
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "IndentBlanklineUnfocused", { fg = "#1d1f21" })
+      vim.api.nvim_set_hl(0, "IndentBlanklineFocused", { fg = "#404447" })
+    end)
+    require("ibl").setup({
+      indent = { highlight = { "IndentBlanklineUnfocused" } },
+      scope = { highlight = { "IndentBlanklineFocused" } },
+    })
+  end,
+}
