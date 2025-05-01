a5a52ea5c6fdf2469a67b21ae7d705bb999445e8
theetah <phantomporpoise@gmail.com>
2025-04-30 19:11:32 -0500

    remove redundant functionality
    (i did not know * finds the current word under the cursor)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,11 +19,6 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
--- Custom quick fix keybind
-
-vim.keymap.set("n", "<leader>n", "ye/<C-r>+<CR>", { desc = "Find next match underneath the cursor" })
-vim.keymap.set("n", "<leader><S-n>", "ye/<C-r>+<CR><S-n><S-n>", { desc = "Find previous match underneath the cursor" })
-
 -- PLUGIN SPECIFIC
 
 vim.keymap.set("n", "<leader>bd", "<cmd>bdelete!<CR>", { desc = "Delete the current buffer" })
