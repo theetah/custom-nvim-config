ec64c507a0bebbc3c6e33b3d91729da8bf0dbb4f
theetah <phantomporpoise@gmail.com>
2025-07-04 11:25:56 -0500

    change statusline lsp icons
    might change again later

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,19 +1,21 @@
 -- credit to shadmansaleh and glepnir for this lsp snippet, straight from evil_lualine
+local icon_inactive = "󰲜  "
+local icon_active = "󰱔  "
 local lsp = {
   function()
     local msg = "No LSP Active"
     local buf_ft = vim.api.nvim_get_option_value("filetype", { buf = 0 })
     local clients = vim.lsp.get_clients()
     if next(clients) == nil then
-      return "  " .. msg
+      return icon_inactive .. msg
     end
     for _, client in ipairs(clients) do
       local filetypes = client.config.filetypes
       if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        return "  " .. client.name
+        return icon_active .. client.name
       end
     end
-    return "  " .. msg
+    return icon_inactive .. msg
   end,
 }
 
