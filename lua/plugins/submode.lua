aabbc7c8c9d069b0129ef719de550645b0248c0c
theetah <theetah.github@proton.me>
2025-09-02 23:41:52 -0500

    Improve debug submode functionality & readd vim-sleuth

--- lua/plugins/submode.lua
+++ lua/plugins/submode.lua
@@ -5,6 +5,7 @@ return {
     "folke/which-key.nvim",
     "nvim-neotest/nvim-nio",
     "rcarriga/nvim-dap-ui",
+    "theHamsta/nvim-dap-virtual-text",
   },
   lazy = true,
   version = "6.0.0",
@@ -17,6 +18,7 @@ return {
     local dap = require("dap")
     local wk = require("which-key")
     local dapui = require("dapui")
+    local dapvt = require("nvim-dap-virtual-text.virtual_text")
     submode.create("Debug", {
       mode = "n",
       enter = "<leader>d",
@@ -25,10 +27,13 @@ return {
     wk.add({ "<leader>d", desc = "Debug submode" })
     submode.set("Debug", "b", dap.toggle_breakpoint, { desc = "Toggle a breakpoint" })
     submode.set("Debug", "c", dap.continue, { desc = "Start/Continue the debugging process" })
-    submode.set("Debug", "s", function()
+    submode.set("Debug", "B", dap.clear_breakpoints, { desc = "Clear all breakpoints" })
+    submode.set("Debug", "S", function()
       dap.close()
       dap.clear_breakpoints()
       dapui.close()
+      dapvt.clear_virtual_text()
+      vim.api.nvim_input("<ESC>")
       print("Aborted debugging process.")
     end, { desc = "Stop the debugging process, clear all breakpoints, and close all DAP UI windows" })
     submode.set("Debug", "w", function()
@@ -39,7 +44,7 @@ return {
       else
         print("No watches window was found.")
       end
-    end, { desc = "Focus the watches buffer, if it exists" })
+    end, { desc = "Focus the watches buffer and input a new watch variable, if it exists" })
     submode.set("Debug", "<Left>", dap.step_back, { desc = "Debug: step back" })
     submode.set("Debug", "<Right>", dap.step_over, { desc = "Debug: step over" })
     submode.set("Debug", "<Up>", dap.step_out, { desc = "Debug: step out" })
