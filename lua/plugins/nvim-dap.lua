077d57722379133faa5580297e9901766aacce95
theetah <theetah.github@proton.me>
2025-09-04 23:48:24 -0500

    Clean up nvim-dap implementation

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,34 +1,67 @@
 return {
-  -- This great set of plugins won't work unless I do this file separation terribleness.
-  -- They doesn't seem to like having setups called within one file!
-  "mfussenegger/nvim-dap",
-  config = function()
-    local dap = require("dap")
+  "jay-babu/mason-nvim-dap.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    {
+      {
+        "mfussenegger/nvim-dap",
+        config = function()
+          -- Keymaps
+          -- vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle a breakpoint" })
+          -- vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue the debugging process" })
 
-    -- Keymaps
-    -- vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle a breakpoint" })
-    -- vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue the debugging process" })
+          -- Change what nvim-dap uses for its signs
+          vim.fn.sign_define("DapBreakpoint", {
+            text = "",
+            texthl = "DiagnosticSignError",
+            linehl = "",
+            numhl = "",
+          })
 
-    -- Change what nvim-dap uses for its signs
-    vim.fn.sign_define("DapBreakpoint", {
-      text = "",
-      texthl = "DiagnosticSignError",
-      linehl = "",
-      numhl = "",
-    })
+          vim.fn.sign_define("DapBreakpointRejected", {
+            text = "",
+            texthl = "DiagnosticSignError",
+            linehl = "",
+            numhl = "",
+          })
 
-    vim.fn.sign_define("DapBreakpointRejected", {
-      text = "",
-      texthl = "DiagnosticSignError",
-      linehl = "",
-      numhl = "",
-    })
+          vim.fn.sign_define("DapStopped", {
+            text = "󱕊",
+            texthl = "DiagnosticSignWarn",
+            linehl = "Visual",
+            numhl = "DiagnosticSignWarn",
+          })
+        end,
+      },
+      {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "nvim-neotest/nvim-nio" },
+        config = function()
+          local dap = require("dap")
+          local dapui = require("dapui")
+          dapui.setup()
 
-    vim.fn.sign_define("DapStopped", {
-      text = "󱕊",
-      texthl = "DiagnosticSignWarn",
-      linehl = "Visual",
-      numhl = "DiagnosticSignWarn",
-    })
-  end,
+          -- Listeners
+          dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+          end
+          dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+          end
+          dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+          end
+          dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+          end
+        end,
+      },
+      {
+        "theHamsta/nvim-dap-virtual-text",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        opts = { commented = true },
+      },
+    },
+  },
+  opts = { handlers = {} },
 }
--- lua/plugins/satellite.lua
+++ lua/plugins/satellite.lua
@@ -1,3 +1,1 @@
-return {
-  "lewis6991/satellite.nvim",
-}
+return { "lewis6991/satellite.nvim" }
